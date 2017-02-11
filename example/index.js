55805c74583ee9893eed237fb75d9d30046a6c57
Kelly, Kevin J [Axios] <kkelly2@lgsinnovations.com>
2017-02-10 19:33:08 -0500

    version 0.0.2

--- example/index.js
+++ example/index.js
@@ -1,5 +1,5 @@
 import  App  from './app';
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+require('file-loader?name=demo.html!./demo.html');
 ReactDOM.render(<App />, document.getElementById('app'));
