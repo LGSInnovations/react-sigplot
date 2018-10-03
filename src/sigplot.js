1a31f7bc9b80ab6b51388b2e5cc82721b1e645f9
Michael Recachinas <mgr3yp@virginia.edu>
2018-10-03 21:55:50 +0100

    Remove unused props in SigPlot component

--- src/sigplot.js
+++ src/sigplot.js
@@ -10,22 +10,12 @@ export default class SigPlot extends Component {
 
   getChildContext() {
     return {
-      plot: this.plot
+      plot: this.plot,
     };
   }
 
   componentDidMount() {
-    const {
-      data,
-      file,
-      raster,
-      options,
-      xdelta,
-      xunits,
-      yunits,
-      type,
-      websocket,
-    } = this.props;
+    const { options } = this.props;
     this.plot = new Plot(this.element, options);
 
     // Have to trigger context tree, setting state does that.
@@ -65,28 +55,16 @@ SigPlot.propTypes = {
   children: PropTypes.node,
   height: PropTypes.number,
   width: PropTypes.number,
-  file: PropTypes.string,
-  websocket: PropTypes.string,
-  data: PropTypes.arrayOf(PropTypes.number),
-  raster: PropTypes.bool,
   options: PropTypes.object,
-  type: PropTypes.number,
-  xunits: PropTypes.number,
-  yunits: PropTypes.number,
-  xdelta: PropTypes.number
 };
 
 SigPlot.defaultProps = {
   height: 300,
   width: 300,
-  type: 2000,
-  xdelta: 1,
-  xunits: 3, // Hz
-  yunits: 26, // 10*log
   options: {
     all: true,
     expand: true,
     autol: 100,
     autohide_panbars: true
-  }
+  },
 };
