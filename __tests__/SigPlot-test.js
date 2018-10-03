4c77f50c486631bda6acc0f591d8d1eb0ade37fb
Michael Recachinas <mgr3yp@virginia.edu>
2018-10-03 21:56:33 +0100

    Add more comprehensive tests

--- __tests__/SigPlot-test.js
+++ __tests__/SigPlot-test.js
@@ -3,19 +3,193 @@ import { expect } from 'chai';
 import { configure, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import toJson from 'enzyme-to-json';
-import { SigPlot } from '../src/index.js';
+import { SigPlot, ArrayLayer, PipeLayer } from '../src/index.js';
 
 configure({ adapter: new Adapter() })
 
 describe('<SigPlot />', () => {
-  it('renders with empty data prop', () => {
-    const component = mount(<SigPlot data={[]} />);
+  it('renders with no child layer', () => {
+    const options = {
+      all: true,
+      expand: true,
+      autol: 100,
+      autohide_panbars: true,
+    };
+    const component = mount(<SigPlot />);
+    expect(component.props().width).to.equal(300);
+    expect(component.props().height).to.equal(300);
+    expect(component.props().options.all).to.equal(true);
+    expect(component.props().options.expand).to.equal(true);
+    expect(component.props().options.autol).to.equal(100);
+    expect(component.props().options.autohide_panbars).to.equal(true);
+  });
+
+  it('renders with no child layer with custom height and width', () => {
+    const options = {
+      all: true,
+      expand: true,
+      autol: 100,
+      autohide_panbars: true,
+    };
+    const component = mount(<SigPlot height={500} width={800} />);
+    expect(component.props().width).to.equal(800);
+    expect(component.props().height).to.equal(500);
+    expect(component.props().options.all).to.equal(true);
+    expect(component.props().options.expand).to.equal(true);
+    expect(component.props().options.autol).to.equal(100);
+    expect(component.props().options.autohide_panbars).to.equal(true);
+  });
+
+  it('renders with no child layer with custom options and custom height and width', () => {
+    const options = {
+      all: false,
+      expand: false,
+      autol: 1,
+      autohide_panbars: false,
+    };
+    const component = mount(<SigPlot height={500} width={800} options={options} />);
+    expect(component.props().width).to.equal(800);
+    expect(component.props().height).to.equal(500);
+    expect(component.props().options.all).to.equal(false);
+    expect(component.props().options.expand).to.equal(false);
+    expect(component.props().options.autol).to.equal(1);
+    expect(component.props().options.autohide_panbars).to.equal(false);
+  });
+
+  it('SigPlot renders with 1D ArrayLayer with no data', () => {
+    const options = {
+      all: true,
+      expand: true,
+      autol: 100,
+      autohide_panbars: true,
+    };
+    const oneDimensionalData = [];
+    const component = mount(
+      <SigPlot options={{autol: 1}}>
+        <ArrayLayer data={oneDimensionalData} />
+      </SigPlot>
+    );
+    expect(component.props().width).to.equal(300);
+    expect(component.props().height).to.equal(300);
+    expect(component.props().children[0].props().data).to.equal(oneDimensionalData);
+  });
+
+  it('SigPlot renders with 1D ArrayLayer with data', () => {
+    const options = {
+      all: true,
+      expand: true,
+      autol: 100,
+      autohide_panbars: true,
+    };
+    let random = [];
+    for (let i = 0; i <= 1000; i += 1) {
+        random.push(Math.random());
+    }
+    const oneDimensionalData = random;
+    const component = mount(
+      <SigPlot options={{autol: 1}}>
+        <ArrayLayer data={oneDimensionalData} />
+      </SigPlot>
+    );
     expect(toJson(component)).toMatchSnapshot();
   });
 
-  it('SigPlot renders data from array', () => {
+  it('SigPlot renders with 2D ArrayLayer with data', () => {
+    const options = {
+      all: true,
+      expand: true,
+      autol: 100,
+      autohide_panbars: true,
+    };
+    let random = [];
+    for (let i = 0; i <= 1000; i += 1) {
+        random.push(Math.random());
+    }
+    const oneDimensionalData = random;
     const component = mount(
-        <SigPlot data={[1, 2, 3, 4]} />
+        <SigPlot options={{autol: 1}}>
+          <ArrayLayer data={oneDimensionalData} />
+        </SigPlot>
+    );
+    expect(toJson(component)).toMatchSnapshot();
+  });
+
+  it('SigPlot renders with 1D PipeLayer with no data', () => {
+    const options = {
+      all: true,
+      expand: true,
+      autol: 100,
+      autohide_panbars: true,
+    };
+    let random = [];
+    for (let i = 0; i <= 1000; i += 1) {
+        random.push(Math.random());
+    }
+    const oneDimensionalData = random;
+    const component = mount(
+        <SigPlot options={{autol: 1}}>
+          <ArrayLayer data={oneDimensionalData} />
+        </SigPlot>
+    );
+    expect(toJson(component)).toMatchSnapshot();
+  });
+
+  it('SigPlot renders with 1D PipeLayer with data', () => {
+    const options = {
+      all: true,
+      expand: true,
+      autol: 100,
+      autohide_panbars: true,
+    };
+    let random = [];
+    for (let i = 0; i <= 1000; i += 1) {
+        random.push(Math.random());
+    }
+    const oneDimensionalData = random;
+    const component = mount(
+        <SigPlot options={{autol: 1}}>
+          <ArrayLayer data={oneDimensionalData} />
+        </SigPlot>
+    );
+    expect(toJson(component)).toMatchSnapshot();
+  });
+
+  it('SigPlot renders with 2D PipeLayer with no data', () => {
+    const options = {
+      all: true,
+      expand: true,
+      autol: 100,
+      autohide_panbars: true,
+    };
+    let random = [];
+    for (let i = 0; i <= 1000; i += 1) {
+        random.push(Math.random());
+    }
+    const oneDimensionalData = random;
+    const component = mount(
+        <SigPlot options={{autol: 1}}>
+          <ArrayLayer data={oneDimensionalData} />
+        </SigPlot>
+    );
+    expect(toJson(component)).toMatchSnapshot();
+  });
+
+  it('SigPlot renders with 2D PipeLayer with data', () => {
+    const options = {
+      all: true,
+      expand: true,
+      autol: 100,
+      autohide_panbars: true,
+    };
+    let random = [];
+    for (let i = 0; i <= 1000; i += 1) {
+        random.push(Math.random());
+    }
+    const oneDimensionalData = random;
+    const component = mount(
+        <SigPlot options={{autol: 1}}>
+          <ArrayLayer data={oneDimensionalData} />
+        </SigPlot>
     );
     expect(toJson(component)).toMatchSnapshot();
   });
