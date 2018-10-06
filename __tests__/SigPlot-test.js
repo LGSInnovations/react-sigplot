f84080ec44abb23b9dc8d72a13f51c3ec18ff0d3
Michael Recachinas <mgr3yp@virginia.edu>
2018-10-06 13:26:49 +0100

    Fix href tests

--- __tests__/SigPlot-test.js
+++ __tests__/SigPlot-test.js
@@ -12,6 +12,8 @@ import {
 
 configure({ adapter: new Adapter() })
 
+window.alert = (msg) => { console.log(msg) };
+
 describe('<SigPlot />', () => {
   it('renders with no child layer', () => {
     const options = {
@@ -64,7 +66,7 @@ describe('<SigPlot />', () => {
     expect(component.props().children).to.be.undefined;
   });
 
-  it('SigPlot renders with 1D ArrayLayer with no data', () => {
+  it('renders with 1D ArrayLayer with no data', () => {
     const options = {
       all: true,
       expand: true,
@@ -87,7 +89,7 @@ describe('<SigPlot />', () => {
     expect(component.props().children.props.data).to.equal(oneDimensionalData);
   });
 
-  it('SigPlot renders with 2 1D ArrayLayers with no data', () => {
+  it('renders with 2 1D ArrayLayers with no data', () => {
     const options = {
       all: true,
       expand: true,
@@ -114,7 +116,7 @@ describe('<SigPlot />', () => {
     expect(component.props().children[1].props.data).to.equal(oneDimensionalData2);
   });
 
-  it('SigPlot renders with 1D ArrayLayer with data', () => {
+  it('renders with 1D ArrayLayer with data', () => {
     const options = {
       all: true,
       expand: true,
@@ -141,7 +143,7 @@ describe('<SigPlot />', () => {
     expect(component.props().children.props.data).to.equal(oneDimensionalData);
   });
 
-  it('SigPlot renders with 2 1D ArrayLayers with data', () => {
+  it('renders with 2 1D ArrayLayers with data', () => {
     const options = {
       all: true,
       expand: true,
@@ -179,7 +181,7 @@ describe('<SigPlot />', () => {
     expect(component.props().children[1].props.data).to.equal(oneDimensionalData2);
   });
 
-  it('SigPlot renders with 2D ArrayLayer with no data', () => {
+  it('renders with 2D ArrayLayer with no data', () => {
     const options = {
       all: true,
       expand: true,
@@ -207,7 +209,7 @@ describe('<SigPlot />', () => {
     expect(component.props().children.props.data).to.equal(twoDimensionalData);
   });
 
-  it('SigPlot renders with 2D ArrayLayer with data', () => {
+  it('renders with 2D ArrayLayer with data', () => {
     const options = {
       all: true,
       expand: true,
@@ -238,7 +240,25 @@ describe('<SigPlot />', () => {
     expect(component.props().children.props.data).to.equal(twoDimensionalData);
   });
 
-  it('SigPlot renders with 1D PipeLayer with no data', () => {
+  it('throws with 1D PipeLayer with no data and no size defined', () => {
+    expect(() => {
+      const options = {
+        all: true,
+        expand: true,
+        autol: 100,
+        autohide_panbars: true,
+      };
+      let random = [];
+      const oneDimensionalData = random;
+      const component = mount(
+        <SigPlot options={options}>
+          <PipeLayer data={oneDimensionalData} />
+        </SigPlot>
+      );
+    }).to.throw(/1D layer could not determine appropriate size for pipe, use framesize option/);
+  });
+
+  it('renders with 1D PipeLayer with no data', () => {
     const options = {
       all: true,
       expand: true,
@@ -249,7 +269,58 @@ describe('<SigPlot />', () => {
     const oneDimensionalData = random;
     const component = mount(
         <SigPlot options={options}>
-          <PipeLayer data={oneDimensionalData} />
+          <PipeLayer data={oneDimensionalData} options={{framesize: 1000, type: 1000, subsize: 1000}} />
+        </SigPlot>
+    );
+    expect(component.props().width).to.equal(300);
+    expect(component.props().height).to.equal(300);
+    expect(component.props().options.all).to.equal(true);
+    expect(component.props().options.expand).to.equal(true);
+    expect(component.props().options.autol).to.equal(100);
+    expect(component.props().options.autohide_panbars).to.equal(true);
+    expect(component.props().children).to.be.an('object');
+    expect(component.props().children.props.data).to.equal(oneDimensionalData);
+  });
+
+  it('renders with 1D PipeLayer with data', () => {
+    const options = {
+      all: true,
+      expand: true,
+      autol: 100,
+      autohide_panbars: true,
+    };
+    let random = [];
+    for (let i = 0; i <= 1000; i += 1) {
+        random.push(Math.random());
+    }
+    const oneDimensionalData = random;
+    const component = mount(
+        <SigPlot options={options}>
+          <PipeLayer data={oneDimensionalData} options={{framesize: 1000, type: 1000, subsize: 1000}} />
+        </SigPlot>
+    );
+    expect(component.props().width).to.equal(300);
+    expect(component.props().height).to.equal(300);
+    expect(component.props().options.all).to.equal(true);
+    expect(component.props().options.expand).to.equal(true);
+    expect(component.props().options.autol).to.equal(100);
+    expect(component.props().options.autohide_panbars).to.equal(true);
+    expect(component.props().children).to.be.an('object');
+    expect(component.props().children.props.data).to.equal(oneDimensionalData);
+  });
+
+  it('renders with 2D PipeLayer with no data', () => {
+    const options = {
+      all: true,
+      expand: true,
+      autol: 100,
+      autohide_panbars: true,
+    };
+    let random = [];
+    const twoDimensionalData = random;
+    const component = mount(
+        <SigPlot options={options}>
+          <PipeLayer data={twoDimensionalData} options={{type: 2000, subsize: 1000}} />
         </SigPlot>
     );
     expect(component.props().width).to.equal(300);
@@ -262,7 +333,7 @@ describe('<SigPlot />', () => {
     expect(component.props().children.props.data).to.equal(twoDimensionalData);
   });
 
-  it('SigPlot renders with 1D PipeLayer with data', () => {
+  it('renders with 2D PipeLayer with data', () => {
     const options = {
       all: true,
       expand: true,
@@ -273,132 +344,115 @@ describe('<SigPlot />', () => {
     for (let i = 0; i <= 1000; i += 1) {
         random.push(Math.random());
     }
-    const oneDimensionalData = random;
+    const twoDimensionalData = random;
     const component = mount(
         <SigPlot options={options}>
-          <PipeLayer data={oneDimensionalData} />
+          <PipeLayer data={twoDimensionalData} options={{type: 2000, subsize: 1000}} />
         </SigPlot>
     );
-    expect(toJson(component)).toMatchSnapshot();
+    expect(component.props().width).to.equal(300);
+    expect(component.props().height).to.equal(300);
+    expect(component.props().options.all).to.equal(true);
+    expect(component.props().options.expand).to.equal(true);
+    expect(component.props().options.autol).to.equal(100);
+    expect(component.props().options.autohide_panbars).to.equal(true);
+    expect(component.props().children).to.be.an('object');
+    expect(component.props().children.props.data).to.equal(twoDimensionalData);
   });
 
-  it('SigPlot renders with 2D PipeLayer with no data', () => {
+  it('renders with HrefLayer with no file', () => {
     const options = {
       all: true,
       expand: true,
       autol: 100,
       autohide_panbars: true,
     };
-    let random = [];
-    for (let i = 0; i <= 1000; i += 1) {
-        random.push(Math.random());
-    }
-    const oneDimensionalData = random;
+    const file = "";
     const component = mount(
-        <SigPlot options={options}>
-          <PipeLayer data={oneDimensionalData} />
-        </SigPlot>
+      <SigPlot options={options}>
+        <HrefLayer />
+      </SigPlot>
     );
-    expect(toJson(component)).toMatchSnapshot();
+    expect(component.props().width).to.equal(300);
+    expect(component.props().height).to.equal(300);
+    expect(component.props().options.all).to.equal(true);
+    expect(component.props().options.expand).to.equal(true);
+    expect(component.props().options.autol).to.equal(100);
+    expect(component.props().options.autohide_panbars).to.equal(true);
+    expect(component.props().children).to.be.an('object');
+    expect(component.props().children.props.href).to.equal(file);
   });
 
-  it('SigPlot renders with 2D PipeLayer with data', () => {
+  it('renders with HrefLayer with valid href', () => {
     const options = {
       all: true,
       expand: true,
       autol: 100,
       autohide_panbars: true,
     };
-    let random = [];
-    for (let i = 0; i <= 1000; i += 1) {
-        random.push(Math.random());
-    }
-    const oneDimensionalData = random;
+    const file = "dat/penny.prm";
     const component = mount(
-        <SigPlot options={options}>
-          <PipeLayer data={oneDimensionalData} />
-        </SigPlot>
+      <SigPlot options={options}>
+        <HrefLayer href={file} />
+      </SigPlot>
     );
-    expect(toJson(component)).toMatchSnapshot();
+    expect(component.props().width).to.equal(300);
+    expect(component.props().height).to.equal(300);
+    expect(component.props().options.all).to.equal(true);
+    expect(component.props().options.expand).to.equal(true);
+    expect(component.props().options.autol).to.equal(100);
+    expect(component.props().options.autohide_panbars).to.equal(true);
+    expect(component.props().children).to.be.an('object');
+    expect(component.props().children.props.href).to.equal(file);
   });
 
-  it('SigPlot renders with 1D HrefLayer with no file', () => {
+  it('renders with HrefLayer with invalid href', () => {
     const options = {
       all: true,
       expand: true,
       autol: 100,
       autohide_panbars: true,
     };
-    let random = [];
-    for (let i = 0; i <= 1000; i += 1) {
-        random.push(Math.random());
-    }
-    const oneDimensionalData = random;
+    const file = "dat/invalid.prm";
     const component = mount(
-        <SigPlot options={options}>
-          <HrefLayer data={oneDimensionalData} />
-        </SigPlot>
+      <SigPlot options={options}>
+        <HrefLayer href={file} />
+      </SigPlot>
     );
-    expect(toJson(component)).toMatchSnapshot();
+    expect(component.props().width).to.equal(300);
+    expect(component.props().height).to.equal(300);
+    expect(component.props().options.all).to.equal(true);
+    expect(component.props().options.expand).to.equal(true);
+    expect(component.props().options.autol).to.equal(100);
+    expect(component.props().options.autohide_panbars).to.equal(true);
+    expect(component.props().children).to.be.an('object');
+    expect(component.props().children.props.href).to.equal(file);
   });
 
-  it('SigPlot renders with 1D HrefLayer with valid href', () => {
+  it('renders with HrefLayer with 2 valid hrefs', () => {
     const options = {
       all: true,
       expand: true,
       autol: 100,
       autohide_panbars: true,
     };
-    let random = [];
-    for (let i = 0; i <= 1000; i += 1) {
-        random.push(Math.random());
-    }
-    const oneDimensionalData = random;
+    const file1 = "dat/penny.prm";
+    const file2 = "dat/sin.tmp";
     const component = mount(
-        <SigPlot options={options}>
-          <HrefLayer data={oneDimensionalData} />
-        </SigPlot>
+      <SigPlot options={options}>
+        <HrefLayer href={file1} />
+        <HrefLayer href={file2} />
+      </SigPlot>
     );
-    expect(toJson(component)).toMatchSnapshot();
-  });
-
-  it('SigPlot renders with 1D HrefLayer with 2 valid hrefs', () => {
-    const options = {
-      all: true,
-      expand: true,
-      autol: 100,
-      autohide_panbars: true,
-    };
-    let random = [];
-    for (let i = 0; i <= 1000; i += 1) {
-        random.push(Math.random());
-    }
-    const oneDimensionalData = random;
-    const component = mount(
-        <SigPlot options={options}>
-          <HrefLayer data={oneDimensionalData} />
-        </SigPlot>
-    );
-    expect(toJson(component)).toMatchSnapshot();
-  });
-
-  it('SigPlot renders with 2D HrefLayer with valid href', () => {
-    const options = {
-      all: true,
-      expand: true,
-      autol: 100,
-      autohide_panbars: true,
-    };
-    let random = [];
-    for (let i = 0; i <= 1000; i += 1) {
-        random.push(Math.random());
-    }
-    const oneDimensionalData = random;
-    const component = mount(
-        <SigPlot options={options}>
-          <HrefLayer data={oneDimensionalData} />
-        </SigPlot>
-    );
-    expect(toJson(component)).toMatchSnapshot();
+    expect(component.props().width).to.equal(300);
+    expect(component.props().height).to.equal(300);
+    expect(component.props().options.all).to.equal(true);
+    expect(component.props().options.expand).to.equal(true);
+    expect(component.props().options.autol).to.equal(100);
+    expect(component.props().options.autohide_panbars).to.equal(true);
+    expect(component.props().children).to.be.an('array');
+    expect(component.props().children).to.have.lengthOf(2);
+    expect(component.props().children[0].props.href).to.equal(file1);
+    expect(component.props().children[1].props.href).to.equal(file2);
   });
 });
