40e108e14e10ac4304b8cbf88e56128dca61a0f3
Michael Recachinas <mgr3yp@virginia.edu>
2018-10-25 19:24:20 +0100

    chore: Add layer-specific tests

--- __tests__/SigPlot.test.js
+++ __tests__/SigPlot.test.js
@@ -4,6 +4,7 @@ import sinon from 'sinon';
 import { configure, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import toJson from 'enzyme-to-json';
+import { Plot } from 'sigplot';
 import {
   SigPlot,
   ArrayLayer,
@@ -214,6 +215,52 @@ describe('<SigPlot />', () => {
     expect(component.instance().plot._Gx.lyr[0].ypoint).to.eql(new Float64Array(oneDimensionalData));
   });
 
+  it('renders with 1D ArrayLayer with data then re-renders', () => {
+    const options = {
+      all: true,
+      expand: true,
+      autol: 100,
+      autohide_panbars: true,
+    };
+    let random1 = [];
+    for (let i = 0; i <= 1000; i += 1) {
+        random1.push(i * 10);
+    }
+    let random2 = [];
+    for (let i = 0; i <= 1000; i += 1) {
+        random2.push(i * 10);
+    }
+    const oneDimensionalData1 = random1;
+    const oneDimensionalData2 = random2;
+
+    let childInstance = {};
+    const component = mount(
+      <SigPlot options={options}>
+        <ArrayLayer data={oneDimensionalData1} />
+      </SigPlot>
+    );
+    expect(component.props().width).to.equal(300);
+    expect(component.props().height).to.equal(300);
+    expect(component.props().options.all).to.equal(true);
+    expect(component.props().options.expand).to.equal(true);
+    expect(component.props().options.autol).to.equal(100);
+    expect(component.props().options.autohide_panbars).to.equal(true);
+    expect(component.props().children).to.be.an('object');
+    expect(component.props().children.props.data).to.equal(oneDimensionalData1);
+    expect(component.find('div').prop('style').width).to.equal(300);
+    expect(component.find('div').prop('style').height).to.equal(300);
+    expect(component.find('div').prop('style').display).to.equal('inline-block');
+    expect(component.instance().plot).to.not.be.undefined;
+    expect(component.instance().plot._Gx.all).to.equal(true);
+    expect(component.instance().plot._Gx.expand).to.equal(true);
+    expect(component.instance().plot._Gx.autol).to.equal(100);
+    expect(component.instance().plot._Gx.autohide_panbars).to.equal(true);
+    expect(component.instance().plot._Gx.lyr).to.have.lengthOf(1);
+    expect(component.instance().plot._Gx.lyr[0].options).to.be.an('object').that.is.empty;
+    expect(component.instance().plot._Gx.lyr[0].ypoint).to.have.lengthOf(oneDimensionalData1.length);
+    expect(component.instance().plot._Gx.lyr[0].ypoint).to.eql(new Float64Array(oneDimensionalData1));
+  });
+
   it('renders with 2 1D ArrayLayers with data', () => {
     const options = {
       all: true,
@@ -367,57 +414,6 @@ describe('<SigPlot />', () => {
     }).to.throw(/1D layer could not determine appropriate size for pipe, use framesize option/);
   });
 
-  it('renders with 1D PipeLayer with no data', () => {
-    const options = {
-      all: true,
-      expand: true,
-      autol: 100,
-      autohide_panbars: true,
-    };
-    let random = [];
-    const oneDimensionalData = random;
-    const component = mount(
-        <SigPlot options={options}>
-          <PipeLayer data={oneDimensionalData} options={{framesize: 1000, type: 1000, subsize: 1000}} />
-        </SigPlot>
-    );
-    expect(component.props().width).to.equal(300);
-    expect(component.props().height).to.equal(300);
-    expect(component.props().options.all).to.equal(true);
-    expect(component.props().options.expand).to.equal(true);
-    expect(component.props().options.autol).to.equal(100);
-    expect(component.props().options.autohide_panbars).to.equal(true);
-    expect(component.props().children).to.be.an('object');
-    expect(component.props().children.props.data).to.equal(oneDimensionalData);
-  });
-
-  it('renders with 1D PipeLayer with data', () => {
-    const options = {
-      all: true,
-      expand: true,
-      autol: 100,
-      autohide_panbars: true,
-    };
-    let random = [];
-    for (let i = 0; i <= 1000; i += 1) {
-        random.push(Math.random());
-    }
-    const oneDimensionalData = random;
-    const component = mount(
-        <SigPlot options={options}>
-          <PipeLayer data={oneDimensionalData} options={{framesize: 1000, type: 1000, subsize: 1000}} />
-        </SigPlot>
-    );
-    expect(component.props().width).to.equal(300);
-    expect(component.props().height).to.equal(300);
-    expect(component.props().options.all).to.equal(true);
-    expect(component.props().options.expand).to.equal(true);
-    expect(component.props().options.autol).to.equal(100);
-    expect(component.props().options.autohide_panbars).to.equal(true);
-    expect(component.props().children).to.be.an('object');
-    expect(component.props().children.props.data).to.equal(oneDimensionalData);
-  });
-
   it('renders with 2D PipeLayer with no data', () => {
     const options = {
       all: true,
@@ -440,9 +436,17 @@ describe('<SigPlot />', () => {
     expect(component.props().options.autohide_panbars).to.equal(true);
     expect(component.props().children).to.be.an('object');
     expect(component.props().children.props.data).to.equal(twoDimensionalData);
+    expect(component.instance().plot._Gx.all).to.equal(true);
+    expect(component.instance().plot._Gx.expand).to.equal(true);
+    expect(component.instance().plot._Gx.autol).to.equal(100);
+    expect(component.instance().plot._Gx.autohide_panbars).to.equal(true);
+    expect(component.instance().plot._Gx.lyr).to.have.lengthOf(1);
+    expect(component.instance().plot._Gx.lyr[0].hcb.subsize).to.equal(1000);
+    expect(component.instance().plot._Gx.lyr[0].hcb.type).to.equal(2000);
+    expect(component.instance().plot._Gx.lyr[0].zbuf).to.eql(new Float64Array(1000));
   });
 
-  it('renders with 2D PipeLayer with data', () => {
+  it('renders with 2D PipeLayer with data passed in but not displayed', () => {
     const options = {
       all: true,
       expand: true,
@@ -451,7 +455,7 @@ describe('<SigPlot />', () => {
     };
     let random = [];
     for (let i = 0; i <= 1000; i += 1) {
-        random.push(Math.random());
+        random.push(10 * i);
     }
     const twoDimensionalData = random;
     const component = mount(
@@ -467,6 +471,14 @@ describe('<SigPlot />', () => {
     expect(component.props().options.autohide_panbars).to.equal(true);
     expect(component.props().children).to.be.an('object');
     expect(component.props().children.props.data).to.equal(twoDimensionalData);
+    expect(component.instance().plot._Gx.all).to.equal(true);
+    expect(component.instance().plot._Gx.expand).to.equal(true);
+    expect(component.instance().plot._Gx.autol).to.equal(100);
+    expect(component.instance().plot._Gx.autohide_panbars).to.equal(true);
+    expect(component.instance().plot._Gx.lyr).to.have.lengthOf(1);
+    expect(component.instance().plot._Gx.lyr[0].hcb.subsize).to.equal(1000);
+    expect(component.instance().plot._Gx.lyr[0].hcb.type).to.equal(2000);
+    expect(component.instance().plot._Gx.lyr[0].zbuf).to.eql(new Float64Array(1000));
   });
 
   it('renders with HrefLayer with no file', () => {
@@ -563,5 +575,232 @@ describe('<SigPlot />', () => {
     expect(component.props().children).to.have.lengthOf(2);
     expect(component.props().children[0].props.href).to.equal(file1);
     expect(component.props().children[1].props.href).to.equal(file2);
+  });
+});
+
+describe('<ArrayLayer />', () => {
+  it('reloads plot on data prop change', () => {
+    const options = {};
+    const element = global.document.createElement("div");
+    const context = { plot: new Plot(element, options) };
+
+    let random = [];
+    for (let i = 0; i <= 1000; i += 1) {
+        random.push(i * 10);
+    }
+    const oneDimensionalData = random;
+
+    const component = mount(
+      <ArrayLayer data={oneDimensionalData} />,
+      { context }
+    ); 
+
+    expect(component.props().data).to.equal(oneDimensionalData);
+    expect(component.instance().plot).to.not.be.undefined;
+    expect(component.instance().plot._Gx.all).to.equal(false);
+    expect(component.instance().plot._Gx.expand).to.equal(false);
+    expect(component.instance().plot._Gx.autol).to.equal(-1);
+    expect(component.instance().plot._Gx.lyr).to.have.lengthOf(1);
+    expect(component.instance().plot._Gx.lyr[0].options).to.be.an('object').that.is.empty;
+    expect(component.instance().plot._Gx.lyr[0].ypoint).to.have.lengthOf(oneDimensionalData.length);
+    expect(component.instance().plot._Gx.lyr[0].ypoint).to.eql(new Float64Array(oneDimensionalData));
+
+    let random2 = [];
+    for (let i = 0; i <= 1000; i += 1) {
+        random2.push(i * 10);
+    }
+    const oneDimensionalData2 = random2;
+
+    component.setProps({data: random2});
+    expect(component.props().data).to.equal(oneDimensionalData2);
+    expect(component.instance().plot).to.not.be.undefined;
+    expect(component.instance().plot._Gx.all).to.equal(false);
+    expect(component.instance().plot._Gx.expand).to.equal(false);
+    expect(component.instance().plot._Gx.autol).to.equal(-1);
+    expect(component.instance().plot._Gx.lyr).to.have.lengthOf(1);
+    expect(component.instance().plot._Gx.lyr[0].options).to.be.an('object').that.is.empty;
+    expect(component.instance().plot._Gx.lyr[0].ypoint).to.have.lengthOf(oneDimensionalData2.length);
+    expect(component.instance().plot._Gx.lyr[0].ypoint).to.eql(new Float64Array(oneDimensionalData2));
+  });
+});
+
+describe('<PipeLayer />', () => {
+  it('pushes new data to plot on data prop change', () => {
+    const element = global.document.createElement("div");
+    const context = { plot: new Plot(element, {}) };
+
+    const twoDimensionalData = [];
+
+    const options = {framesize: 1000, type: 2000, subsize: 1000};
+    const component = mount(
+      <PipeLayer data={twoDimensionalData} options={options} />,
+      { context }
+    ); 
+
+    expect(component.props().data).to.equal(twoDimensionalData);
+    expect(component.instance().plot._Gx.all).to.equal(false);
+    expect(component.instance().plot._Gx.expand).to.equal(false);
+    expect(component.instance().plot._Gx.autol).to.equal(-1);
+    expect(component.instance().plot._Gx.lyr).to.have.lengthOf(1);
+    expect(component.instance().plot._Gx.lyr[0].hcb.subsize).to.equal(1000);
+    expect(component.instance().plot._Gx.lyr[0].hcb.type).to.equal(2000);
+    const tmp = new Float64Array(1000);
+    for (let i = 0; i < 1000; i++) {
+      expect(component.instance().plot._Gx.lyr[0].hcb.dview[i]).to.equal(tmp[i]);
+    }
+
+    let random = [];
+    for (let i = 0; i <= 1000; i += 1) {
+        random.push(i * 10);
+    }
+    component.setProps({data: random});
+    expect(component.props().data).to.equal(random);
+    expect(component.instance().plot._Gx.all).to.equal(false);
+    expect(component.instance().plot._Gx.expand).to.equal(false);
+    expect(component.instance().plot._Gx.autol).to.equal(-1);
+    expect(component.instance().plot._Gx.lyr).to.have.lengthOf(1);
+    expect(component.instance().plot._Gx.lyr[0].hcb.subsize).to.equal(1000);
+    expect(component.instance().plot._Gx.lyr[0].hcb.type).to.equal(2000);
+    const tmp2 = new Float64Array(random);
+    for (let i = 0; i < 1000; i++) {
+      expect(component.instance().plot._Gx.lyr[0].hcb.dview[i]).to.equal(tmp2[i]);
+    }
+  });
+});
+
+describe('<HrefLayer />', () => {
+  it('reloads plot on href prop change', () => {
+    const element = global.document.createElement("div");
+    const context = { plot: new Plot(element, {}) };
+    const hrefOne = "";
+    const component = mount(
+      <HrefLayer href={hrefOne} />,
+      { context }
+    ); 
+
+    expect(component.props().href).to.equal(hrefOne);
+    expect(component.instance().plot).to.not.be.undefined;
+    expect(component.instance().plot._Gx.all).to.equal(false);
+    expect(component.instance().plot._Gx.expand).to.equal(false);
+    expect(component.instance().plot._Gx.autol).to.equal(-1);
+    expect(component.instance().plot._Gx.lyr).to.have.lengthOf(1);
+    expect(component.instance().plot._Gx.lyr[0].options).to.be.an('object').that.is.empty;
+    expect(component.instance().plot._Gx.lyr[0].hcb.file_name).to.equal(hrefTwo);
+
+    const hrefTwo = "dat/penny.prm";
+    component.setProps({href: hrefTwo});
+    expect(component.props().href).to.equal(hrefTwo);
+    expect(component.instance().plot).to.not.be.undefined;
+    expect(component.instance().plot._Gx.all).to.equal(false);
+    expect(component.instance().plot._Gx.expand).to.equal(false);
+    expect(component.instance().plot._Gx.autol).to.equal(-1);
+    expect(component.instance().plot._Gx.lyr).to.have.lengthOf(1);
+    expect(component.instance().plot._Gx.lyr[0].options).to.be.an('object').that.is.empty;
+    expect(component.instance().plot._Gx.lyr[0].hcb.file_name).to.equal(hrefTwo);
+  });
+
+  it('changes settings on options prop change', () => {
+    const element = global.document.createElement("div");
+    const context = { plot: new Plot(element, {}) };
+    const hrefOne = "dat/penny.prm";
+    const component = mount(
+      <HrefLayer href={hrefOne} />,
+      { context }
+    ); 
+
+    expect(component.props().href).to.equal(hrefOne);
+    expect(component.instance().plot).to.not.be.undefined;
+    expect(component.instance().plot._Gx.all).to.equal(false);
+    expect(component.instance().plot._Gx.expand).to.equal(false);
+    expect(component.instance().plot._Gx.autol).to.equal(-1);
+    expect(component.instance().plot._Gx.lyr).to.have.lengthOf(1);
+    expect(component.instance().plot._Gx.lyr[0].options).to.be.an('object').that.is.empty;
+    expect(component.instance().plot._Gx.lyr[0].hcb.file_name).to.equal(hrefOne);
+
+    const options = {
+      xmin: 0,
+      xmax: 100,
+    };
+    component.setProps({options: options});
+    expect(component.props().href).to.equal(hrefOne);
+    expect(component.instance().plot).to.not.be.undefined;
+    expect(component.instance().plot._Gx.all).to.equal(false);
+    expect(component.instance().plot._Gx.expand).to.equal(false);
+    expect(component.instance().plot._Gx.autol).to.equal(-1);
+    expect(component.instance().plot._Gx.lyr).to.have.lengthOf(1);
+    expect(component.instance().plot._Gx.lyr[0].options).to.be.an('object').that.is.not.empty;
+    expect(component.instance().plot._Gx.lyr[0].options).to.equal(options);
+    expect(component.instance().plot._Gx.lyr[0].hcb.file_name).to.equal(hrefOne);
+  });
+});
+
+describe('<WebsocketLayer />', () => {
+  it('reloads plot on wsurl prop change', () => {
+    const options = {};
+    const element = global.document.createElement("div");
+    const context = { plot: new Plot(element, options) };
+
+    const websocketURL = "";
+    const component = mount(
+      <WebsocketLayer wsurl={websocketURL} />,
+      { context }
+    ); 
+
+    expect(component.props().data).to.equal(oneDimensionalData);
+    expect(component.instance().plot).to.not.be.undefined;
+    expect(component.instance().plot._Gx.all).to.equal(false);
+    expect(component.instance().plot._Gx.expand).to.equal(false);
+    expect(component.instance().plot._Gx.autol).to.equal(-1);
+    expect(component.instance().plot._Gx.lyr).to.have.lengthOf(1);
+    expect(component.instance().plot._Gx.lyr[0].options).to.be.an('object').that.is.empty;
+    expect(component.instance().plot._Gx.lyr[0].ypoint).to.have.lengthOf(oneDimensionalData.length);
+
+    let random2 = [];
+    for (let i = 0; i <= 1000; i += 1) {
+        random2.push(i * 10);
+    }
+    const oneDimensionalData2 = random2;
+
+    component.setProps({data: random2});
+    expect(component.props().data).to.equal(oneDimensionalData2);
+    expect(component.instance().plot).to.not.be.undefined;
+    expect(component.instance().plot._Gx.all).to.equal(false);
+    expect(component.instance().plot._Gx.expand).to.equal(false);
+    expect(component.instance().plot._Gx.autol).to.equal(-1);
+    expect(component.instance().plot._Gx.lyr).to.have.lengthOf(1);
+    expect(component.instance().plot._Gx.lyr[0].options).to.be.an('object').that.is.empty;
+    expect(component.instance().plot._Gx.lyr[0].ypoint).to.have.lengthOf(oneDimensionalData2.length);
+  });
+
+  it('changes settings on options prop change', () => {
+    const element = global.document.createElement("div");
+    const context = { plot: new Plot(element, options) };
+
+    const websocketURL = "";
+    const component = mount(
+      <WebsocketLayer wsurl={websocketURL} />,
+      { context }
+    ); 
+
+    expect(component.props().data).to.equal(oneDimensionalData);
+    expect(component.instance().plot).to.not.be.undefined;
+    expect(component.instance().plot._Gx.all).to.equal(false);
+    expect(component.instance().plot._Gx.expand).to.equal(false);
+    expect(component.instance().plot._Gx.autol).to.equal(-1);
+    expect(component.instance().plot._Gx.lyr).to.have.lengthOf(1);
+    expect(component.instance().plot._Gx.lyr[0].options).to.be.an('object').that.is.empty;
+    // expect(component.instance().plot._Gx.lyr[0].ypoint).to.have.lengthOf(oneDimensionalData.length);
+
+    const options = {xmin: 0, xmax: 100};
+    component.setProps({options});
+    expect(component.props().data).to.equal(oneDimensionalData2);
+    expect(component.instance().plot).to.not.be.undefined;
+    expect(component.instance().plot._Gx.all).to.equal(false);
+    expect(component.instance().plot._Gx.expand).to.equal(false);
+    expect(component.instance().plot._Gx.autol).to.equal(-1);
+    expect(component.instance().plot._Gx.lyr).to.have.lengthOf(1);
+    expect(component.instance().plot._Gx.lyr[0].options).to.be.an('object').that.is.not.empty;
+    expect(component.instance().plot._Gx.lyr[0].options).to.equal(options);
+    // expect(component.instance().plot._Gx.lyr[0].ypoint).to.have.lengthOf(oneDimensionalData2.length);
   });
 });
