a96c62f131c6ab6ee0bd904a902b66d55015404b
Michael Recachinas <mgr3yp@virginia.edu>
2019-02-10 11:30:42 +0000

    docs: Document sigplot.js

--- src/sigplot.js
+++ src/sigplot.js
@@ -2,7 +2,57 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Plot } from 'sigplot';
 
+/**
+ * SigPlot.js React wrapper class
+ *
+ * @version 0.1.2
+ * @visibleName SigPlot.js React Wrapper
+ */
 export default class SigPlot extends Component {
+  static childContextTypes = {
+    plot: PropTypes.instanceOf(Plot),
+  }
+
+  static propTypes = {
+    /**
+     * Different Layer nodes (e.g., ArrayLayer, PipeLayer, etc.)
+     *
+     * @ignore
+     */
+    children: PropTypes.node,
+
+    /** Height of the wrapper div */
+    height: PropTypes.number,
+
+    /** Width of the wrapper div */
+    width: PropTypes.number,
+
+    /** CSS 'display' property */
+    display: PropTypes.string,
+
+    /** Styles object for any other CSS styles on the wrapper div */
+    styles: PropTypes.object,
+
+    /**
+     * SigPlot plot-level options
+     *
+     * @see See [sigplot.Plot Docs](http://sigplot.lgsinnovations.com/html/doc/sigplot.Plot.html)
+     */
+    options: PropTypes.object,
+  }
+
+  static defaultProps = {
+    height: 300,
+    width: 300,
+    display: 'inline-block',
+    options: {
+      all: true,
+      expand: true,
+      autol: 100,
+      autohide_panbars: true
+    },
+  }
+
   constructor(props) {
     super(props);
     this.state = {};
@@ -25,10 +75,24 @@ export default class SigPlot extends Component {
   }
 
   componentWillReceiveProps(nextProps) {
-    const { height, width } = this.props;
-    const { height: newHeight, width: newWidth } = nextProps;
+    const { height, width, options } = this.props;
+    const {
+      height: newHeight,
+      width: newWidth,
+      options: newOptions
+    } = nextProps;
+
+    // When the outer div height/width changes,
+    // we need to explicitly tell SigPlot to resize;
+    // otherwise, it won't resize automatically.
     if (newHeight !== height || newWidth !== width) {
       this.plot.checkresize();
+    }
+
+    // If the options change at the plot level,
+    // we need to handle that
+    if (newOptions !== options) {
+      this.plot.change_settings(newOptions);
     }
   }
 
@@ -41,6 +105,25 @@ export default class SigPlot extends Component {
       children: propChildren,
     } = this.props;
     const { plot } = this;
+
+    /**
+     * Recall we're treating the `sigplot.layer1d` and
+     * `sigplot.layer2d` as (virtual) children nodes since
+     * they are simply manipulations/API calls that modify
+     * the underlying <canvas>.
+     *
+     * As such, the user should never have to access the
+     * `children` property outright, instead being able to
+     * write
+     *
+     *   <SigPlot>
+     *     <ArrayLayer ... />
+     *   </SigPlot>
+     *
+     * Anyway, the point of the following statement is
+     * to provide the `plot` object (controlled by the parent)
+     * to the child so it can mutate it.
+     */
     const children = plot
       ? React.Children.map(propChildren, (child) => {
         if (child) {
@@ -54,7 +137,7 @@ export default class SigPlot extends Component {
         style={{
           height,
           width,
-          display,
+          display, // this will be deprecated
           ...styles
         }}
         ref={element => this.element = element}
@@ -64,28 +147,3 @@ export default class SigPlot extends Component {
     );
   }
 }
-
-SigPlot.childContextTypes = {
-  plot: PropTypes.instanceOf(Plot)
-};
-
-SigPlot.propTypes = {
-  children: PropTypes.node,
-  height: PropTypes.number,
-  width: PropTypes.number,
-  display: PropTypes.string,
-  styles: PropTypes.object,
-  options: PropTypes.object,
-};
-
-SigPlot.defaultProps = {
-  height: 300,
-  width: 300,
-  display: 'inline-block',
-  options: {
-    all: true,
-    expand: true,
-    autol: 100,
-    autohide_panbars: true
-  },
-};
