ed81e814ed8fde723f5ec286a78283a652060459
Sean Sullivan <deseanjunk@yahoo.com>
2019-05-10 16:55:39 -0400

    Add bluelayer (#11)
    * Create bluelayer.js
    
    * export bluelayer
    
    * overlay bluefile doesn't take options, only layer options

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ export { default as SigPlot } from './sigplot';
 export { default as ArrayLayer } from './arraylayer';
 export { default as PipeLayer } from './pipelayer';
 export { default as HrefLayer } from './hreflayer';
+export { default as BlueLayer } from './bluelayer';
 export { default as WebsocketLayer } from './websocketlayer';
